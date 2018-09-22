a78bb4ef0dd8520874a6a2bd6ee5bf1224d0cd31
ThinkPowerful <sudheendran.lakshman@gmail.com>
2018-09-22 17:45:04 +0530

    added author

--- src/test/java/qmetry/ValidateLoginTest.java
+++ src/test/java/qmetry/ValidateLoginTest.java
@@ -4,10 +4,13 @@ import org.testng.annotations.Test;
 
 public class ValidateLoginTest {
 	
+	//author
+	
 	@Test
 	public void validateLogin()
 	{
 		System.out.println("Login Validated");
 	}
+	
 
 }
