30ecf0f677ed96127e22d8183e7a2099dc88cd2c
ThinkPowerful <sudheendran.lakshman@gmail.com>
2018-09-23 10:31:29 +0530

    new test added

---
+++ src/test/java/qmetry/ValidateContactTest.java
@@ -0,0 +1,13 @@
+package qmetry;
+
+import org.testng.annotations.Test;
+
+public class ValidateContactTest {
+	
+	@Test
+	public void validateTitle()
+	{
+		System.out.println("contact is Validated");
+	}
+
+}
