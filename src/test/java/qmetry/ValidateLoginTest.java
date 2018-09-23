2e30d05687de0e40d283cd026c477b3bf9e0a004
ThinkPowerful <sudheendran.lakshman@gmail.com>
2018-09-23 13:05:45 +0530

    t

--- src/test/java/qmetry/ValidateLoginTest.java
+++ src/test/java/qmetry/ValidateLoginTest.java
@@ -1,16 +1,31 @@
 package qmetry;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class ValidateLoginTest {
 	
 	//author
 	
 	@Test
-	public void validateLogin()
+	public void validateLogin() throws IOException
 	{
+		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("D:\\San\\Testing Guide\\E2E\\Reports\\extent.html");
+		htmlReporter.setAppendExisting(false);
+		ExtentReports extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+		ExtentTest test = extent.createTest("MyFirstTest");
 		System.out.println("Login Validated");
 		System.setProperty("webdriver.gecko.driver","D:\\San\\Testing Guide\\E2E\\exe files\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
@@ -24,6 +39,8 @@ public class ValidateLoginTest {
 
         // launch Fire fox and direct it to the Base URL
         driver.get(baseUrl);
+        
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 
         // get the actual value of the title
         actualTitle = driver.getTitle();
@@ -33,14 +50,21 @@ public class ValidateLoginTest {
          * the result as "Passed" or "Failed"
          */
         if (actualTitle.contentEquals(expectedTitle)){
-            System.out.println("Test Passed!");
+            System.out.println("Test is passing!");
+            test.pass("Test Passed!");
+            FileUtils.copyFile(scrFile, new File("D:\\San\\Testing Guide\\E2E\\Reports\\Snapshots\\screenshot.png"));
+            test.addScreenCaptureFromPath("screenshot.png");
         } else {
             System.out.println("Test Failed");
+            test.fail("Test Failed");
         }
        
+        extent.flush();
         //close Fire fox
         driver.close();
 	}
+	
+	
 	
 
 }
